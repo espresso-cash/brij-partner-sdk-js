36c15f5d7d2a8afe98c59376512fd963ab241b80
Vlad Sumin <vladsum00@gmail.com>
2024-08-27 18:50:13 +0300

    upd

--- partner_client.js
+++ partner_client.js
@@ -9,173 +9,181 @@ const { encode: encodeBase58, decode: decodeBase58 } = base58;
 const { encode: base64encode, decode: base64decode } = base64;
 
 class KycPartnerClient {
-  constructor({ authKeyPair, baseUrl }) {
-    this.authKeyPair = authKeyPair;
-    this.baseUrl = baseUrl;
+      constructor({ authKeyPair, baseUrl }) {
+            this.authKeyPair = authKeyPair;
+            this.baseUrl = baseUrl;
 
-    this._authPublicKey = '';
-    this._token = '';
-    this._apiClient = null;
-    this._secretBox = null;
-    this._signingKey = null;
-  }
+            this._authPublicKey = '';
+            this._token = '';
+            this._apiClient = null;
+            this._secretBox = null;
+            this._signingKey = null;
+      }
 
-  async init({ partnerToken, secretKey }) {
-    await this._generateAuthToken(partnerToken);
-    await this._initializeEncryption(secretKey);
-  }
+      async init({ partnerToken, secretKey }) {
+            await this._generateAuthToken(partnerToken);
+            await this._initializeEncryption(secretKey);
+      }
 
-  async _initializeEncryption(secretKey) {
-    const secretKeyBytes = decodeBase58(secretKey);
-    this._secretBox = new SecretBox(Uint8Array.from(secretKeyBytes));
-    const authPrivateKey = await this.authKeyPair.getPrivateKeyBytes();
-    this._signingKey = nacl.sign.keyPair.fromSeed(
-      Uint8Array.from([...authPrivateKey, ...decodeBase58(this._authPublicKey)])
-    );
-  }
+      async _initializeEncryption(secretKey) {
+            const secretKeyBytes = decodeBase58(secretKey);
+            this._secretBox = new SecretBox(Uint8Array.from(secretKeyBytes));
+            const authPrivateKey = await this.authKeyPair.getPrivateKeyBytes();
+            this._signingKey = nacl.sign.keyPair.fromSeed(
+                  Uint8Array.from([...authPrivateKey, ...decodeBase58(this._authPublicKey)])
+            );
+      }
 
-  async _generateAuthToken(partnerToken) {
-    const authPublicKey = await this.authKeyPair.getPublicKeyBytes();
-    this._authPublicKey = encodeBase58(authPublicKey);
+      async _generateAuthToken(partnerToken) {
+            this._authPublicKey = encodeBase58(await this.authKeyPair.getPublicKeyBytes());
 
-    const tokenData = { delegated: partnerToken };
-    const privateKey = Uint8Array.from([
-      ...await this.authKeyPair.getPrivateKeyBytes(),
-      ...decodeBase58(this._authPublicKey),
-    ]);
+            // Construct the JWT payload and sign it using EdDSA
+            const partnerTokenData = {
+                  delegated: partnerToken,
+                  issuer: this._authPublicKey
+            };
 
-    this._token = jwt.sign(tokenData, nacl.sign.keyPair.fromSeed(privateKey).secretKey, { algorithm: 'EdDSA', issuer: this._authPublicKey });
+            const privateKeyBytes = Uint8Array.from([
+                  ...await this.authKeyPair.getPrivateKeyBytes(),
+                  ...decodeBase58(this._authPublicKey)
+            ]);
 
-    const instance = axios.create({
-      baseURL: this.baseUrl,
-    });
-    instance.interceptors.request.use(config => {
-      config.headers['Authorization'] = `Bearer ${this._token}`;
-      return config;
-    });
-    this._apiClient = instance;
-  }
+            // Sign the token
+            this._token = jwt.sign(partnerTokenData, Buffer.from(privateKeyBytes), {
+                  // algorithm: 'ES256',
+            });
 
-  async getData({ userPK, secretKey }) {
-    const response = await this._apiClient.get('/kycServiceGetData');
-    const responseData = response.data;
 
-    const verifyKey = nacl.sign.keyPair.fromSeed(decodeBase58(userPK)).publicKey;
-    const box = new SecretBox(Uint8Array.from(decodeBase58(secretKey)));
+            const instance = axios.create({
+                  baseURL: this.baseUrl,
+            });
+            instance.interceptors.request.use(config => {
+                  config.headers['Authorization'] = `Bearer ${this._token}`;
+                  return config;
+            });
+            this._apiClient = instance;
+      }
 
-    const data = await Promise.all(
-      Object.entries(responseData).map(async ([key, value]) => {
-        const signedDataRaw = value;
-        if (!signedDataRaw) return [key, ''];
+      async getData({ userPK, secretKey }) {
+            const response = await this._apiClient.get('/kycServiceGetData');
+            const responseData = response.data;
 
-        const signedMessage = nacl.sign.open(
-          base64decode(signedDataRaw),
-          verifyKey
-        );
+            const verifyKey = nacl.sign.keyPair.fromSeed(decodeBase58(userPK)).publicKey;
+            const box = new SecretBox(Uint8Array.from(decodeBase58(secretKey)));
 
-        if (!signedMessage) {
-          throw new Error(`Invalid signature for key: ${key}`);
-        }
+            const data = await Promise.all(
+                  Object.entries(responseData).map(async ([key, value]) => {
+                        const signedDataRaw = value;
+                        if (!signedDataRaw) return [key, ''];
 
-        const encryptedData = base64encode(signedMessage);
-        const decrypted = box.decrypt(new TextDecoder().decode(base64decode(encryptedData)));
+                        const signedMessage = nacl.sign.open(
+                              base64decode(signedDataRaw),
+                              verifyKey
+                        );
 
-        return [key, ['photoSelfie', 'photoIdCard'].includes(key) ? decrypted : new TextDecoder().decode(decrypted)];
-      })
-    );
+                        if (!signedMessage) {
+                              throw new Error(`Invalid signature for key: ${key}`);
+                        }
 
-    return Object.fromEntries(data);
-  }
+                        const encryptedData = base64encode(signedMessage);
+                        const decrypted = box.decrypt(new TextDecoder().decode(base64decode(encryptedData)));
 
-  async setValidationResult({ value }) {
-    const encryptedValue = value.encryptAndSign(this._encryptAndSign.bind(this));
+                        return [key, ['photoSelfie', 'photoIdCard'].includes(key) ? decrypted : new TextDecoder().decode(decrypted)];
+                  })
+            );
 
-    await this._apiClient.post('/kycServiceSetValidationResult', { data: encryptedValue });
-  }
+            return Object.fromEntries(data);
+      }
 
-  async getValidationResult({ key, validatorPK, secretKey }) {
-    const response = await this._apiClient.post('/kycServiceGetValidationResult', {
-      publicKey: validatorPK,
-    });
-    const data = response.data[key];
+      async setValidationResult({ value }) {
+            const encryptedValue = value.encryptAndSign(this._encryptAndSign.bind(this));
 
-    if (!data) return null;
+            await this._apiClient.post('/kycServiceSetValidationResult', { data: encryptedValue });
+      }
 
-    const box = new SecretBox(Uint8Array.from(decodeBase58(secretKey)));
-    const signedMessage = nacl.sign.open(base64decode(data));
-    const encryptedData = base64encode(signedMessage);
-    const decrypted = box.decrypt(new TextDecoder().decode(base64decode(encryptedData)));
+      async getValidationResult({ key, validatorPK, secretKey }) {
+            const response = await this._apiClient.post('/kycServiceGetValidationResult', {
+                  publicKey: validatorPK,
+            });
+            const data = response.data[key];
 
-    return new TextDecoder().decode(decrypted);
-  }
+            if (!data) return null;
 
-  async validateField(value) {
-    const updatedEmail = value.email != null ? await this._hash(value.email) : null;
-    const updatedPhone = value.phone != null ? await this._hash(value.phone) : null;
+            const box = new SecretBox(Uint8Array.from(decodeBase58(secretKey)));
+            const signedMessage = nacl.sign.open(base64decode(data));
+            const encryptedData = base64encode(signedMessage);
+            const decrypted = box.decrypt(new TextDecoder().decode(base64decode(encryptedData)));
 
-    const updatedValue = Object.assign({}, value, {
-      email: updatedEmail,
-      phone: updatedPhone,
-    });
+            return new TextDecoder().decode(decrypted);
+      }
 
-    await this.setValidationResult({ value: updatedValue });
-  }
+      async validateField(value) {
+            const updatedEmail = value.email != null ? await this._hash(value.email) : null;
+            const updatedPhone = value.phone != null ? await this._hash(value.phone) : null;
 
-  async _hash(value) {
-    const hash = createHash('blake2b512').update(value).digest('hex');
-    return hash;
-  }
+            const updatedValue = Object.assign({}, value, {
+                  email: updatedEmail,
+                  phone: updatedPhone,
+            });
 
-  _encryptAndSign(data) {
-    const encrypted = this._secretBox.encrypt(data);
-    return nacl.sign(encrypted, this._signingKey.secretKey);
-  }
+            await this.setValidationResult({ value: updatedValue });
+      }
+
+      async _hash(value) {
+            const hash = createHash('blake2b512').update(value).digest('hex');
+            return hash;
+      }
+
+      _encryptAndSign(data) {
+            const encrypted = this._secretBox.encrypt(data);
+            return nacl.sign(encrypted, this._signingKey.secretKey);
+      }
 }
 
 class V1ValidationData {
-  constructor({ email, phone, kycSmileId }) {
-    this.email = email;
-    this.phone = phone;
-    this.kycSmileId = kycSmileId;
-  }
+      constructor({ email, phone, kycSmileId }) {
+            this.email = email;
+            this.phone = phone;
+            this.kycSmileId = kycSmileId;
+      }
 
-  encryptAndSign(encryptAndSignFunction) {
-    return new V1ValidationData({
-      email: this._encryptAndEncode(this.email, encryptAndSignFunction),
-      phone: this._encryptAndEncode(this.phone, encryptAndSignFunction),
-      kycSmileId: this._encryptAndEncode(this.kycSmileId, encryptAndSignFunction),
-    });
-  }
+      encryptAndSign(encryptAndSignFunction) {
+            return new V1ValidationData({
+                  email: this._encryptAndEncode(this.email, encryptAndSignFunction),
+                  phone: this._encryptAndEncode(this.phone, encryptAndSignFunction),
+                  kycSmileId: this._encryptAndEncode(this.kycSmileId, encryptAndSignFunction),
+            });
+      }
 
-  _encryptAndEncode(value, encryptAndSignFunction) {
-    if (!value) return null;
-    const encryptedData = encryptAndSignFunction(new TextEncoder().encode(value));
-    return base64encode(encryptedData);
-  }
+      _encryptAndEncode(value, encryptAndSignFunction) {
+            if (!value) return null;
+            const encryptedData = encryptAndSignFunction(new TextEncoder().encode(value));
+            return base64encode(encryptedData);
+      }
 }
 
 class SecretBox {
-  constructor(key) {
-    this.key = key;
-  }
+      constructor(key) {
+            this.key = key;
+      }
 
-  encrypt(data) {
-    const iv = randomBytes(12);
-    const cipher = createCipheriv('aes-256-gcm', this.key, iv);
-    const encrypted = Buffer.concat([cipher.update(data), cipher.final()]);
-    const tag = cipher.getAuthTag();
-    return Buffer.concat([iv, encrypted, tag]);
-  }
+      encrypt(data) {
+            const iv = randomBytes(12);
+            const cipher = createCipheriv('aes-256-gcm', this.key, iv);
+            const encrypted = Buffer.concat([cipher.update(data), cipher.final()]);
+            const tag = cipher.getAuthTag();
+            return Buffer.concat([iv, encrypted, tag]);
+      }
 
-  decrypt(data) {
-    const iv = data.slice(0, 12);
-    const tag = data.slice(data.length - 16);
-    const encrypted = data.slice(12, data.length - 16);
+      decrypt(data) {
+            const iv = data.slice(0, 12);
+            const tag = data.slice(data.length - 16);
+            const encrypted = data.slice(12, data.length - 16);
 
-    const decipher = createDecipheriv('aes-256-gcm', this.key, iv);
-    decipher.setAuthTag(tag);
-    return Buffer.concat([decipher.update(encrypted), decipher.final()]);
-  }
+            const decipher = createDecipheriv('aes-256-gcm', this.key, iv);
+            decipher.setAuthTag(tag);
+            return Buffer.concat([decipher.update(encrypted), decipher.final()]);
+      }
 }
 
 export { KycPartnerClient, V1ValidationData };
